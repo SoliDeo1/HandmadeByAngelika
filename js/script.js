a9a22e60a07dcb0a40e5634c9cd456bba91f39ae
SoliDeo1 <hgafk1988@gmail.com>
2025-06-26 22:13:24 +0300

    fighting with caroussel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
-});
+/* $(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1, // или 2, 3, сколько нужно
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+    });
+}); */
