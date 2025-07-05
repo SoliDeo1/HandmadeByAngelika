49b377c3c3ea19cc073dc8be4201678f5bc2a739
SoliDeo1 <hgafk1988@gmail.com>
2025-07-05 14:26:24 +0300

    added burger menu

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function flipImage(card) {
     card.classList.toggle("flipped");
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector(".nav-container ul");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("open");
+    });
+});
