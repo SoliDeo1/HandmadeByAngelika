e3395eb3d0559126579cdce4e3e05e7fc1aaa699
SoliDeo1 <hgafk1988@gmail.com>
2025-07-26 00:56:20 +0300

    added styles for burger menu

--- js/script.js
+++ js/script.js
@@ -4,9 +4,17 @@ function flipImage(card) {
 
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector(".burger");
-    const navList = document.querySelector(".nav-container ul");
-
+    const navList = document.querySelector("nav ul");
+    const overlay = document.querySelector(".overlay");
+  
     burger.addEventListener("click", () => {
-        navList.classList.toggle("open");
+      navList.classList.toggle("open");
+      overlay.classList.toggle("active");
     });
-});
+  
+    overlay.addEventListener("click", () => {
+      navList.classList.remove("open");
+      overlay.classList.remove("active");
+    });
+  });
+  
